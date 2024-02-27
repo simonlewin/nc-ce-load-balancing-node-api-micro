6d652df7b074fc9617ac0414eafc4974f70c31a4
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-02-27 15:10:18 +0000

    fix: formatting

--- modules/app_servers/main.tf
+++ modules/app_servers/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "app_servers" {
   count = length(var.servers)
 
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  vpc_security_group_ids      = var.vpc_security_group_ids
-  key_name                    = var.key_name
-  subnet_id                   = var.subnets[count.index]
-  
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  key_name               = var.key_name
+  subnet_id              = var.subnets[count.index]
+
   associate_public_ip_address = true
 
   tags = {
--- modules/app_servers/variables.tf
+++ modules/app_servers/variables.tf
@@ -1,32 +1,32 @@
 variable "servers" {
-  type    = list(string)
+  type        = list(string)
   description = "The list of servers for the instance"
 }
 
 variable "instance_type" {
-  type    = string
+  type        = string
   description = "The instance type for the instance"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "vpc_security_group_ids" {
-  type = list(string)
+  type        = list(string)
   description = "List of security group IDs to associate with."
 }
 
 variable "key_name" {
-  type    = string
+  type        = string
   description = "Key name of the Key Pair to use for the instance"
-  default = "nc-photo-display-app"
+  default     = "nc-photo-display-app"
 }
 
 variable "ami" {
-  type    = string
+  type        = string
   description = "AMI to use for the instance"
-  default = "ami-0e5f882be1900e43b"
+  default     = "ami-0e5f882be1900e43b"
 }
 
 variable "subnets" {
-  type = list(string)
-  description = " VPC Subnet ID to launch in"
+  type        = list(string)
+  description = "VPC Subnet ID to launch in"
 }
