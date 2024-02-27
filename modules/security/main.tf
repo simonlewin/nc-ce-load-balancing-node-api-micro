234069f796c299e178da39f772547267f1811d01
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-02-27 12:25:22 +0000

    feat: add security group rule for port 3000
    add ingress rule for http on port 3000

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -16,6 +16,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
   to_port           = 80
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_port_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_http.id
 
